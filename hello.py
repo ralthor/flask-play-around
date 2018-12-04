2fd4829b36b6151f21985119f3c6d4c2389b18ab
ral <ralthor5@gmail.com>
2018-12-04 16:09:31 +0330

    added templates and started bootstrap

--- hello.py
+++ hello.py
@@ -1,13 +1,42 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Rand Althor',
+		'title': 'My first blog post',
+		'content': 'It is the very start of my blog',
+		'date_posted': '3 December 2018'
+	},
+	{
+		'author': 'John Doe',
+		'title': 'My another blog post',
+		'content': 'I am professional in writing blogs',
+		'date_posted': '2 December 2018'
+	},
+	{
+		'author': 'Corey Schafer',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'April 20, 2018'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'April 21, 2018'
+	}
+]
 
 @app.route('/')
 def index():
 	return 'Index page'
 
 @app.route('/hello')
-def hello():
-	return 'Hello World!'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+	return render_template('hello.html', name=name, title='test')
 	
 @app.route('/user/<username>')
 def profile(username):
@@ -16,6 +45,10 @@ def profile(username):
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
 	return 'Subpath is %s' % subpath
+	
+@app.route('/posts')
+def blog():
+	return render_template('blog.html', posts=posts)
 	
 @app.route('/projects/')
 def projects():
@@ -26,8 +59,3 @@ with app.test_request_context():
 	print(url_for('hello'))
 	print(url_for('hello', next='/'))
 	print(url_for('profile', username='John Doe'))
-	
-#/
-#/hello
-#/hello?next=/
-#/user/John%20Doe
